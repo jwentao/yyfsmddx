036432efd0e3936629800e41874d85c6eaf59653
jinwentao <jinwentao@bigo.sg>
2021-09-12 16:53:19 +0800

    add webrtc demo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const wsInstance = require('express-ws')(app);
 var logger = require('morgan');
 app.use(logger());
 app.use(express.static('public'));
@@ -12,6 +13,17 @@ app.get('/longdd', function (req, res) {
 });
 app.get('/9999', function (req, res) {
     res.redirect('https://www.douyu.com/9999');
+});
+
+app.ws('/rtc', ws => {
+    ws.on('message', data => {
+        // 未做业务处理，收到消息后直接广播
+        wsInstance.getWss().clients.forEach(server => {
+            if (server !== ws) {
+                server.send(data);
+            }
+        });
+    });
 });
 
 var server = app.listen(8888, function () {
